0e1d5ae27281e537ae81fed82b1e9e11458260e2
mmarentette <melrosemarentette@gmail.com>
2023-12-01 11:54:49 -0500

    Delete commented-out code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react';
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 
-import userService from "./utils/userService";
-import { UserProvider } from './contexts/UserContext';
-
 import Layout from './pages/Layout/Layout';
 import FeedPage from './pages/FeedPage/FeedPage';
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import PlantShowPage from './pages/PlantShowPage/PlantShowPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import userService from "./utils/userService";
 
 export default function App() {
   const [user, setUser] = useState(userService.getUser());
@@ -27,7 +25,7 @@ export default function App() {
   if (!user) {
     return (
       <Routes>
-        <Route path="/signup" element={<SignupPage rhandleSignupOrLogin={handleSignupOrLogin} />} />
+        <Route path="/signup" element={<SignupPage handleSignupOrLogin={handleSignupOrLogin} />} />
         <Route path="/login" element={<LoginPage handleSignupOrLogin={handleSignupOrLogin} />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
@@ -35,15 +33,6 @@ export default function App() {
   }
 
   return (
-    // <UserProvider loggedUser={user} handleLogout={logout} >
-    //   <Routes>
-    //     <Route path="/" element={<FeedPage />} />
-    //     <Route path="/signup" element={<SignupPage handleSignupOrLogin={handleSignupOrLogin} />} />
-    //     <Route path="/login" element={<LoginPage handleSignupOrLogin={handleSignupOrLogin} />} />
-    //     <Route path="/plants/:plantId" element={<PlantShowPage />} />
-    //     <Route path="/:username" element={<ProfilePage />} />
-    //   </Routes>
-    // </UserProvider>
     <Routes>
       <Route path="/" element={<Layout loggedUser={user} handleLogout={logout} />}>
         <Route index element={<FeedPage />} />
